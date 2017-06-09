ff9046ebdfa30a0cc8f44dd4a03efe11cb0bf44e
davidwkaiser <davidwkaiser@yahoo.com>
2017-06-08 21:02:31 -0500

    add todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,11 +24,11 @@ export class AppComponent {
     this.todoDataService.addTodo(todo);
   }
 
-  toggleTodoComplete(todo){
+  OnToggleTodoComplete(todo: Todo){
     this.todoDataService.toggleTodoComplete(todo);
   }
 
-  removeTodo(todo){
+  onRemoveTodo(todo: Todo){
     this.todoDataService.deleteTodoById(todo.id);
   }
 
