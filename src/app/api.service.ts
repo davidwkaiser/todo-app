33eb7fa03bd88fe0b89e9d0984a874b505bc47f4
davidwkaiser <davidwkaiser@yahoo.com>
2017-06-24 21:55:58 -0500

    add http method stubs

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'environments/environment';
-import { Http } from '@angular/http';
+
+import { Http, Response } from '@angular/http';
+import { Todo } from './todo';
+import { Observable } from 'rxjs/Observable';
 
 const API_URL = environment.apiUrl;
 
@@ -10,5 +13,28 @@ export class ApiService {
   constructor(
     private http: Http
   ) { }
+//get /todos
+public getAllTodos(){
+  //will use this.http.get()
+}
 
+//post/todos
+public createTodo(todo: Todo){
+  //will use this.http.post()
+}
+
+//get /todos/:id
+public getTodoById(todo: Todo){
+  //will use this.http/get()
+}
+
+//put /todos/:id
+public updateTodo(todo: Todo){
+  //will use this.http.put()
+}
+
+//delete /todos/:id
+public deleteTodoById(todo: Todo){
+  //will use this.http.delete()
+}
 }
