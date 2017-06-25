3a334cc087c70db3a412e7bcf595cc71ccbae3f2
davidwkaiser <davidwkaiser@yahoo.com>
2017-06-24 21:47:44 -0500

    create api service

---
+++ src/app/api.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
+import { Http } from '@angular/http';
+
+const API_URL = environment.apiUrl;
+
+@Injectable()
+export class ApiService {
+
+  constructor(
+    private http: Http
+  ) { }
+
+}
